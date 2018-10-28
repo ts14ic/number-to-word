ca934b3ac7c7c1a61541578e61a041e6ea2485ec
Artyom Yazinsky <iw2snl@gmail.com>
2018-10-28 18:03:37 +0200

    Integrate zero_to_twenty into ones

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-ONES = {
+ZERO_TO_TWENTY = {
+    0: "zero",
     1: "unu",
     2: "doi",
     3: "trei",
@@ -8,7 +9,16 @@ ONES = {
     7: "şapte",
     8: "opt",
     9: "nouă",
-    0: "zero"
+    10: "zece",
+    11: "unsprezece",
+    12: "douăsprezece",
+    13: "treisprezece",
+    14: "patrusprezece",
+    15: "cincisprezece",
+    16: "şasesprezece",
+    17: "şaptesprezece",
+    18: "optsprezece",
+    19: "nouăsprezece",
 }
 
 TENS = {
@@ -21,18 +31,6 @@ TENS = {
     7: "şaptezeci",
     8: "optzeci",
     9: "nouăzeci",
-}
-
-TEN_TO_TWENTY = {
-    11: "unsprezece",
-    12: "douăsprezece",
-    13: "treisprezece",
-    14: "patrusprezece",
-    15: "cincisprezece",
-    16: "şasesprezece",
-    17: "şaptesprezece",
-    18: "optsprezece",
-    19: "nouăsprezece",
 }
 
 HUNDREDS = {
@@ -85,17 +83,14 @@ MILLIONS = {
 
 
 def number_to_text(number: int) -> str:
-    if number < 10:
-        return ONES[number]
+    if number < 20:
+        return ZERO_TO_TWENTY[number]
     elif number < 100:
-        if 10 < number < 20:
-            return TEN_TO_TWENTY[number]
+        tens, ones = divmod(number, 10)
+        if ones != 0:
+            return TENS[tens] + " şi " + ZERO_TO_TWENTY[ones]
         else:
-            tens, ones = divmod(number, 10)
-            if ones != 0:
-                return TENS[tens] + " şi " + ONES[ones]
-            else:
-                return TENS[tens]
+            return TENS[tens]
     elif number < 1_000:
         hundreds, rest = divmod(number, 100)
         if rest != 0:
